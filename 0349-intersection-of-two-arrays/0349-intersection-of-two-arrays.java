ef0de4b34ffcd8f8f7a89584e20f13e9947be37b
Pappu Kumar <appubdm06@gmail.com>
2026-07-24 01:13:49 +0530

    Time: 7 ms (15.34%), Space: 44.5 MB (92.71%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -4,8 +4,7 @@ class Solution {
         for(int i = 0;i<nums1.length;i++){
             for(int j = 0;j<nums2.length;j++){
                 if(nums1[i]==nums2[j]) {
-                    if(ans.contains(nums1[i]));
-                    else{
+                    if(!ans.contains(nums1[i])){
                         ans.add(nums1[i]);
                     }
                 }
