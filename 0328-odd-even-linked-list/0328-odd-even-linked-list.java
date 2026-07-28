222ce021f4ebad0cdfc4ed18ea9af3f1c8cb20a3
Pappu Kumar <appubdm06@gmail.com>
2026-07-28 18:39:17 +0530

    Time: 0 ms (100%), Space: 46.1 MB (90.11%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -1,33 +1,53 @@
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-
         if (head == null || head.next == null)
             return head;
 
-        ListNode dummy1 = new ListNode(-1);
-        ListNode dummy2 = new ListNode(-1);
-        ListNode t1 = dummy1;
-        ListNode t2 = dummy2;
-        ListNode curr = head;
-        int pos = 1;
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
 
-        while (curr != null) {
+        while (even != null && even.next != null) {
 
-            if (pos % 2 != 0) {
-                t1.next = curr;
-                t1 = t1.next;
-            } else {
-                t2.next = curr;
-                t2 = t2.next;
-            }
+            odd.next = even.next;
+            odd = odd.next;
 
-            curr = curr.next;
-            pos++;
+            even.next = odd.next;
+            even = even.next;
         }
 
-        t2.next = null;
-        t1.next = dummy2.next;
+        odd.next = evenHead;
 
-        return dummy1.next;
+        return head;
     }
+    // public ListNode oddEvenList(ListNode head) {
+    //     if (head == null || head.next == null)
+    //         return head;
+
+    //     ListNode dummy1 = new ListNode(-1);
+    //     ListNode dummy2 = new ListNode(-1);
+    //     ListNode t1 = dummy1;
+    //     ListNode t2 = dummy2;
+    //     ListNode curr = head;
+    //     int pos = 1;
+
+    //     while (curr != null) {
+
+    //         if (pos % 2 != 0) {
+    //             t1.next = curr;
+    //             t1 = t1.next;
+    //         } else {
+    //             t2.next = curr;
+    //             t2 = t2.next;
+    //         }
+
+    //         curr = curr.next;
+    //         pos++;
+    //     }
+
+    //     t2.next = null;
+    //     t1.next = dummy2.next;
+
+    //     return dummy1.next;
+    // }
 }
