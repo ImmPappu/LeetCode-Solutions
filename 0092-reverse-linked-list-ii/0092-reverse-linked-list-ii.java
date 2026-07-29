722524bb4aa91dccbd74729de21424b61781a0b1
Pappu Kumar <appubdm06@gmail.com>
2026-07-29 16:52:31 +0530

    Time: 0 ms (100%), Space: 42.9 MB (83.99%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -9,29 +9,72 @@
  * }
  */
 class Solution {
-    public ListNode reverseBetween(ListNode head, int left, int right) {
-        //Approach 1 Array list mei save karke 
-        ArrayList <ListNode> arr = new ArrayList<>();
-        ListNode temp = head;
-        while(temp!=null){
-            arr.add(temp);
-            temp = temp.next;
+    public ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode forw = null;
+        ListNode curr = head;
+        while(curr!=null){
+            forw = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = forw;
         }
-        int i = left-1;
-        int j = right-1;
-        
-        while(i<j){
-           ListNode t1 = arr.get(i);
-           ListNode t2 = arr.get(j);
-            arr.set(i,t2);
-            arr.set(j,t1);
-            i++;
-            j--;
-        }
-        for(i = 0 ;i<arr.size();i++){
-            arr.get(i).next = (i==arr.size()-1) ? null : arr.get(i+1);
-        }
-        return arr.get(0);
+        return prev;
     }
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(head == null || head.next == null || left ==1 && right ==1) return head;
+            ListNode dummy = new ListNode(-1);
+            dummy.next = head;
+            ListNode slow = dummy;
+            ListNode fast = dummy;
+        for(int i = 1;i<left;i++){
+            slow = slow.next;
+        }
+        for(int i = 1;i<=right;i++){
+           fast = fast.next;
+        }
+        ListNode head2 = slow.next;
+        ListNode head3 = fast.next;
+        fast.next = null;
+        slow.next = null;
+       
+        head2 = reverse(head2); 
+        
+        ListNode tail2 = head2;
+
+        while(tail2.next!=null){
+            tail2 = tail2.next;
+        }
+        slow.next = head2 ;
+        tail2.next = head3 ;
+
+        return dummy.next;
+    }
+
+        //Approach 2 Array list mei save karke 
+    //     ArrayList <ListNode> arr = new ArrayList<>();
+    //     ListNode temp = head;
+    //     while(temp!=null){
+    //         arr.add(temp);
+    //         temp = temp.next;
+    //     }
+    //     int i = left-1;
+    //     int j = right-1;
+        
+    //     while(i<j){
+    //        ListNode t1 = arr.get(i);
+    //        ListNode t2 = arr.get(j);
+    //         arr.set(i,t2);
+    //         arr.set(j,t1);
+    //         i++;
+    //         j--;
+    //     }
+    //     for(i = 0 ;i<arr.size();i++){
+    //         arr.get(i).next = (i==arr.size()-1) ? null : arr.get(i+1);
+    //     }
+    //     return arr.get(0);
+    // }
+
+
     }
  
