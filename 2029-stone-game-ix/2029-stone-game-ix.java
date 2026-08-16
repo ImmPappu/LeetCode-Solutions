7834913462272829f286808ce972bc0146a2ea83
Pappu Kumar <appubdm06@gmail.com>
2026-08-17 02:00:48 +0530

    Time: 8 ms (12.58%), Space: 115 MB (15.89%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.java
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -9,14 +9,8 @@ class Solution {
             if(stone%3==1) count1++;
             if(stone%3==2) count2++;
         }
-        if(count1 ==0) {
-            return count2 >= 3 && count0 % 2 == 1;
-        } 
-        if(count2 ==0) {
-            return count1 >= 3 && count0 % 2 == 1;
-        } 
-        if(count0 % 2 ==0){ //Even number of remainder-0 stones means Alice has a winning strategy.
-            return true;
+        if(count0 % 2 ==0){ 
+            return count1 > 0 && count2 > 0;
         }
         return (Math.abs(count1 - count2) > 2); // // If count0 is odd, the difference between type-1 and type-2 stones decides the result.
     }
